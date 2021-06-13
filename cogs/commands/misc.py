7b2dd5473ecded7e14aa054672968080842d6f56
Jlobblet <jlobblet-github@gmx.co.uk>
2021-06-14 00:15:14 +0100

    Start logging stuff

--- cogs/commands/misc.py
+++ cogs/commands/misc.py
@@ -3,32 +3,35 @@ import subprocess
 
 from discord.ext.commands import Bot, Cog, Context, command
 
+from utils.logging import log_func
+
+__all__ = ["Misc"]
+
 logger = logging.getLogger("parnassius.cogs.commands.misc")
+log = log_func(logger)
 
 
 class Misc(Cog):
+    @log
     def __init__(self, bot: Bot):
-        logger.debug(f"{type(self).__name__} __init__ start")
         self.bot = bot
         self.version = subprocess.run(
             ["git", "rev-parse", "--short", "HEAD"], capture_output=True
         ).stdout.decode()
-        logger.debug(f"{type(self).__name__} __init__ end")
 
     @command()
+    @log
     async def ping(self, ctx: Context):
         """Pong!"""
-        logger.debug(f"{self.ping.callback.__name__} start")
         await ctx.send(":ping_pong:")
-        logger.debug(f"{self.ping.callback.__name__} end")
 
     @command()
+    @log
     async def version(self, ctx: Context):
         """Print the hash of the most recent commit."""
-        logger.debug(f"{self.ping.callback.__name__} start")
         await ctx.send(self.version)
-        logger.debug(f"{self.ping.callback.__name__} end")
 
 
+@log
 def setup(bot: Bot):
     bot.add_cog(Misc(bot))
